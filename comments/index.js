1b047aeaddd6cc40850c9ab940033bb7770ac629
Sanketak08 <sakulkarni0801@gmail.com>
2022-08-19 20:17:53 +0530

    Event bus completed

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.get("/posts/:id/comments", (req, res) => {
 });
 
 // To add a new comment
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const { id } = req.params;
@@ -26,9 +27,19 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
   commentsByPostID[id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "COMMENT_ADDED",
+    data: { id: commentId, content, postId: id },
+  });
+
   res.status(201).send(comments);
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.post("/events", (req, res) => {
+  console.log("Event Received: ", req.body.type);
+  res.send({ status: "OK" });
+});
+
+app.listen(4001, () => {
+  console.log("Server is running on port 4001");
 });
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,12 +17,22 @@ app.get("/posts", (req, res) => {
 });
 
 // To create a new post
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "POST_ADDED",
+    data: { id, title },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received: ", req.body.type);
+  res.send({ status: "OK" });
 });
 
 app.listen(4000, () => {
